eb334dac32e345ec4e9802965425fa9c22856831
sedghimohammadamin <Sedghi.mohammadamin.2008@gmail.com>
2025-12-31 20:59:47 +0330

    add view

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  print("Hello Palestine");
+  runApp(const MainMenu());
+}
+
+class MainMenu extends StatefulWidget {
+  const MainMenu({super.key});
+
+  @override
+  State<MainMenu> createState() => _MainMenuState();
+}
+
+class _MainMenuState extends State<MainMenu> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
 }
